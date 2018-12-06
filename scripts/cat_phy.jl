a77d066c3431180b1b7bd415c2c1ff6b765a6cba
Cora Allen-Coleman <6508099+coraallencoleman@users.noreply.github.com>
2018-12-06 10:13:17 -0600

    finished concatenating phy files correctly

--- scripts/cat_phy.jl
+++ scripts/cat_phy.jl
@@ -1,27 +1,54 @@
-files = filter(r".phy$", readdir("/Users/cora/git_repos/NetProject/data/Cui_etal/alignments_1183genes/"))
-# df = vcat([CSV.read(f; header=false, delim=' ', datarow=2) for f in files]) #TODO look for an external process_exited 
-
+files = filter(r"L.*\.phy$", readdir("/Users/cora/git_repos/NetProject/data/Cui_etal/alignments_1183genes/"));
 
 #cd("/Users/cora/git_repos/NetProject/data/Cui_etal/alignments_1183genes")
 #check order of taxa in first file. 
 
 phyDict = Dict{String,String}()
 for f in files
+    println(f)
+    filecounter = 0
     open(f) do f # open for reading by default
-        counter = 0
-        for line in eachline(f)
-            if counter >= 1
-                line = strip(line)
-                dat = split(line," ")
-                # println(dat[1])
-                # println(dat[12])
-                taxon = dat[1]
-                seq = string(dat[12], phyDict[taxon]) #add sequence to current Dict
-                phyDict[taxon] = seq
+        linecounter = 1
+        if filecounter == 0
+            for line in eachline(f)
+                if linecounter > 1 #skips header
+                    line = strip(line)
+                    dat = split(line," ")
+                    taxon = dat[1]
+                    phyDict[string(taxon)] = dat[12] #add sequence to current Dict
+                end
+                linecounter +=1
             end
-            counter += 1
+        else
+            for line in eachline(f)
+                if linecounter > 1 #skips header
+                    line = strip(line)
+                    dat = split(line," ")
+                    taxon = dat[1]
+                    phyDict[string(taxon)] = string(phyDict[taxon], dat[12]) #add sequence to current Dict
+                end
+                linecounter +=1
+            end
         end
+    end
+    filecounter += 1
+end
+
+#write to phy file
+cd("/Users/cora/git_repos/NetProject/data/Cui_etal/alignments_1183genes/")
+outfile = "full.phy"
+open(outfile, "w") do f
+    for key in keys(phyDict)
+        #print without quotes
+        #print(phyDict[key])
+        write(outfile, key, " ", phyDict[key], "\n")
+        #writedlm(outfile, phyDict[key])
     end
 end
 
-
+outfile = "full.phy"
+f = open(outfile, "w")
+for key in keys(phyDict)
+    println(f, key, " ", phyDict[key], "\n")
+end
+close(f)
